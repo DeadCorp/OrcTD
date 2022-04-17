7600cca858194ca05ce8530cbd1bd8cc2d48459c
DeadCorp <hellcroyly@gmail.com>
2022-04-18 01:42:38 +0300

    some test drag and drop

--- scripts/BaseHero.cs
+++ scripts/BaseHero.cs
@@ -4,18 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot.Collections;
 
+[Tool]
 public class BaseHero : Spatial {
     
     private const int Maxlevel = 5;
-    
-    private string _heroType = "no_type";
+
+    public enum HeroTypes {
+        Melee,
+        Range,
+        Mage,
+        Mannequin
+    }
+    private HeroTypes _heroType = HeroTypes.Mannequin;
     public float AttackSpeed { get; set; }
     public float AttackRange { get; set; }
     public float Damage { get; set; }
     public int Level { get; set; }
     
     [Export()] // for test now
-    public string HeroType {
+    public HeroTypes HeroType {
         get => _heroType;
         set {
             _heroType = value;
@@ -26,19 +33,20 @@ public class BaseHero : Spatial {
 
 
     private readonly Dictionary _myData = new Dictionary() {
-        {"HeroType", "range"}, //melee range mage
+        {"HeroType", HeroTypes.Mannequin}, //melee range mage
         {"AttackSpeed", 10.0 },
         {"AttackRange", 10.0 },
         {"Damage", 10.0 },
         {"Level", 1 },
     };
-  
+
+    private DragAndDrop _dragAndDrop = new DragAndDrop();
     public override void _Ready() {
         InitiateHero(_myData);
     }
 
     public void InitiateHero(Dictionary data) {
-        HeroType = (string) data["HeroType"];
+        HeroType = (HeroTypes) data["HeroType"] == HeroTypes.Mannequin ? HeroType : (HeroTypes) data["HeroType"];
         AttackSpeed = (float) data["AttackSpeed"];
         AttackRange = (float) data["AttackRange"];
         Damage = (float) data["Damage"];
@@ -46,7 +54,8 @@ public class BaseHero : Spatial {
 
         GD.Print($" hero : {Name} {{ \n \t HeroType : {HeroType} \n \t AttackSpeed : {AttackSpeed} \n \t AttackRange : {AttackRange}  \n \t Damage : {Damage} \n \t Level : {Level} \n }}");
     }
-
+    
+    
     private void LevelUp() {
         if (Level == Maxlevel) return;
         AttackSpeed *= 1.35f;
@@ -60,23 +69,13 @@ public class BaseHero : Spatial {
     }
     
     private void ChangeHeroType() {
-        if (IsValidType(HeroType)) {
-            Material correctMaterial =
-                ResourceLoader.Load<Material>($"res://materials/test/mannequin_matirial_{HeroType}.tres");
-            if (GetNode("hero/skeleton/mesh") is MeshInstance myMesh && correctMaterial != null) {
-                if (myMesh.GetActiveMaterial(0) != correctMaterial)
-                    myMesh.SetSurfaceMaterial(0, correctMaterial);
-            }
+        Material correctMaterial =
+            ResourceLoader.Load<Material>($"res://materials/test/mannequin_matirial_{HeroType}.tres");
+        if (GetNode("hero/skeleton/mesh") is MeshInstance myMesh && correctMaterial != null) {
+            if (myMesh.GetActiveMaterial(0) != correctMaterial)
+                myMesh.SetSurfaceMaterial(0, correctMaterial);
         }
-    }
-
-    private bool IsValidType(string heroType) {
         
-        var types = new List<string>() {"melee", "range", "mage", "no_type"};
-        foreach (var type in types) {
-            if (type == heroType) return true;
-        }
-
-        return false;
     }
+    
 }
