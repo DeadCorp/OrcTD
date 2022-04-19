9174630cfb23d0a51d0ef4d63dbc6617604ebfb4
DeadCorp <hellcroyly@gmail.com>
2022-04-19 09:54:44 +0300

    add pngs

--- addons/drag_and_drop_c_sharp/DragableCShrap.cs
+++ addons/drag_and_drop_c_sharp/DragableCShrap.cs
@@ -4,20 +4,21 @@ using System;
 [Tool]
 public class DragableCShrap : EditorPlugin
 {
-    public override void _EnterTree() {
-       
-        var texture = GD.Load<Texture>("res://icon.png");
-        
-        var script = GD.Load<Script>("res://addons/drag_and_drop_c_sharp/DragableObject.cs");
-        AddCustomType("DragableObject", "Node", script, texture);
-        
-        script = GD.Load<Script>("res://addons/drag_and_drop_c_sharp/DragableController.cs");
-        AddCustomType("DragableController", "Node", script, texture);
-    }
+	public override void _EnterTree() {
+	   
+		var texture = GD.Load<Texture>("res://icons/hand_cube.svg");
+		
+		var script = GD.Load<Script>("res://addons/drag_and_drop_c_sharp/DragableObject.cs");
+		AddCustomType("DragableObject", "Node", script, texture);
+		
+		texture = GD.Load<Texture>("res://icons/hand.svg");
+		script = GD.Load<Script>("res://addons/drag_and_drop_c_sharp/DragableController.cs");
+		AddCustomType("DragableController", "Node", script, texture);
+	}
 
-    public override void _ExitTree()
-    {
-        RemoveCustomType("DragableObject");
-        RemoveCustomType("DragableController");
-    }
+	public override void _ExitTree()
+	{
+		RemoveCustomType("DragableObject");
+		RemoveCustomType("DragableController");
+	}
 }
